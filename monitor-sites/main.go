e7bea008bd8422422da8bd84e5b3010484d73ee1
Matheus Canovas <matheuscanovas9@gmail.com>
2024-11-05 10:57:05 -0300

    refact: controle de fluxo com switch

--- monitor-sites/main.go
+++ monitor-sites/main.go
@@ -11,13 +11,24 @@ func main() {
 
 	fmt.Println("O comando escolhido foi", comando)
 
-	if comando == 1 {
+	// if comando == 1 {
+	// 	fmt.Println("Monitorando....")
+	// } else if comando == 2 {
+	// 	fmt.Println("Exibindo logs....")
+	// } else if comando == 0 {
+	// 	fmt.Println("Saindo do programa")
+	// } else {
+	// 	fmt.Println("Não conheço este comando")
+	// }
+
+	switch comando {
+	case 1:
 		fmt.Println("Monitorando....")
-	} else if comando == 2 {
+	case 2:
 		fmt.Println("Exibindo logs....")
-	} else if comando == 0 {
+	case 3:
 		fmt.Println("Saindo do programa")
-	} else {
+	default:
 		fmt.Println("Não conheço este comando")
 	}
 }
