8a68dd67df09f4b7acf220017e3993516241eb78
Matheus Canovas <matheuscanovas9@gmail.com>
2024-11-05 10:31:54 -0300

    feat: tipos de variaveis

--- hello/hello.go
+++ hello/hello.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Olá mundo")
-	fmt.Println("Meu primeiro programa em Go!")
+	var nome string = "Matheus"
+	var versao float32 = 1.1
+	var idade int
+	// se o valor da variavel int não é declarada ele assumi o valor 0
+	fmt.Println("Olá, sr.", nome, "sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
 }
