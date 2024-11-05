b863180fb03a4792d1154661b74c39109ed2716c
Matheus Canovas <matheuscanovas9@gmail.com>
2024-11-05 10:35:43 -0300

    feat: inferencia de tipos

--- hello/hello.go
+++ hello/hello.go
@@ -1,12 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
 	var nome string = "Matheus"
+	// nome := "Matheus" atribuição implicita e inferindo tipo dela automaticamente apenas com o :
 	var versao float32 = 1.1
-	var idade int
-	// se o valor da variavel int não é declarada ele assumi o valor 0
+	var idade = 21
 	fmt.Println("Olá, sr.", nome, "sua idade é", idade)
 	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("O tipo da variável nome é", reflect.TypeOf(nome))
+	fmt.Println("O tipo da variável versao é", reflect.TypeOf(versao))
 }
